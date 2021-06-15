1f4fa286fd4fd252c2f644ea087a2bc7f8752705
AndrewMorrow <beardedmongo@gmail.com>
2021-06-15 13:48:17 -0500

    updates validation errors

--- back-end/middleware/customErrorHandler.js
+++ back-end/middleware/customErrorHandler.js
@@ -1,39 +1,38 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 };
 
 // create a custom error
 const createError = (message, errType, errMsg, statusCode) => {
-    const err = new Error(message);
-    err.errors = { [errType]: errMsg };
-    err.statusCode = statusCode;
-    return err;
+  const err = new Error(message);
+  err.errors = { [errType]: errMsg };
+  err.statusCode = statusCode;
+  return err;
 };
 
 // custom error handler
 const errorHandler = (err, req, res, next) => {
-    const statusCode =
-        err.statusCode === undefined || err.statusCode === 200
-            ? 500
-            : err.statusCode;
-
-    res.status(statusCode);
-    res.json({
-        message: err.message,
-        errors: err.errors,
-        stack: process.env.NODE_ENV === "production" ? null : err.stack,
-    });
+  const statusCode =
+    err.statusCode === undefined || err.statusCode === 200
+      ? 500
+      : err.statusCode;
+  console.log("custom error");
+  res.status(statusCode).json({
+    message: err.message,
+    errors: err.errors,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
 };
 
 // catch async errors
 const catchError = (fn) => {
-    return function (req, res, next) {
-        return fn(req, res, next).catch((e) => {
-            next(e);
-        });
-    };
+  return function (req, res, next) {
+    return fn(req, res, next).catch((e) => {
+      next(e);
+    });
+  };
 };
 
 export { notFound, errorHandler, catchError, createError };
--- back-end/validation/register.js
+++ back-end/validation/register.js
@@ -1,42 +1,42 @@
 // Validate Register Info
 export default function validateRegisterInput(data) {
-    let errors = {};
-    const firstTrim = data.firstName.trim();
-    const lastTrim = data.lastName.trim();
-    const emailTrim = data.email.trim();
-    const passTrim = data.password.trim();
-    const pass2Trim = data.password2.trim();
+  let errors = {};
+  const firstTrim = data.firstName.trim();
+  const lastTrim = data.lastName.trim();
+  const emailTrim = data.email.trim();
+  const passTrim = data.password.trim();
+  const pass2Trim = data.password2.trim();
 
-    if (!firstTrim) {
-        errors.firstName = "First name field is required";
-    }
+  if (!firstTrim) {
+    errors.firstName = "First name field is required";
+  }
 
-    if (!lastTrim) {
-        errors.lastName = "Last name field is required";
-    }
+  if (!lastTrim) {
+    errors.lastName = "Last name field is required";
+  }
 
-    if (!emailTrim) {
-        errors.email = "Email field is required";
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailTrim)) {
-        errors.email = "Invalid Email";
-    }
+  if (!emailTrim) {
+    errors.email = "Email field is required";
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailTrim)) {
+    errors.email = "Invalid Email";
+  }
 
-    if (!passTrim) {
-        errors.password = "Password field is required";
-    } else if (!pass2Trim) {
-        errors.password2 = "Please re-enter your password";
-    } else if (passTrim.length < 8 || passTrim.length > 20) {
-        errors.password = "Password must be between 8 and 20 characters";
-    } else if (passTrim !== pass2Trim) {
-        errors.password = "Passwords do not match";
-    }
+  if (!passTrim) {
+    errors.password = "Password field is required";
+  } else if (!pass2Trim) {
+    errors.password2 = "Please re-enter your password";
+  } else if (passTrim.length < 8 || passTrim.length > 20) {
+    errors.password = "Password must be between 8 and 20 characters";
+  } else if (passTrim !== pass2Trim) {
+    errors.password2 = "Passwords do not match";
+  }
 
-    const isValid = Object.keys(errors).length === 0;
+  const isValid = Object.keys(errors).length === 0;
 
-    if (!isValid) {
-        const err = new Error("Invalid Payload");
-        err.statusCode = 422;
-        err.errors = errors;
-        throw err;
-    }
+  if (!isValid) {
+    const err = new Error("Invalid Payload");
+    err.statusCode = 422;
+    err.errors = errors;
+    throw err;
+  }
 }
