2a08dee73a554152cbd09e3e9d9e06083aecd56f
AndrewMorrow <beardedmongo@gmail.com>
2021-06-15 14:22:49 -0500

    updates password errors for login

--- back-end/validation/login.js
+++ back-end/validation/login.js
@@ -1,23 +1,28 @@
 // Validate Login Info
 export default function validateLoginInput(data) {
-    let errors = {};
+  let errors = {};
 
-    if (!data.email.trim()) {
-        errors.email = "Email field is required";
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email.trim())) {
-        errors.email = "Invalid Email";
-    }
+  if (!data.email.trim()) {
+    errors.email = "Email field is required";
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email.trim())) {
+    errors.email = "Invalid Email";
+  }
 
-    if (!data.password.trim()) {
-        errors.password = "Password field is required";
-    }
+  if (!data.password.trim()) {
+    errors.password = "Password field is required";
+  } else if (
+    data.password.trim().length < 8 ||
+    data.password.trim().length > 20
+  ) {
+    errors.password = "Password must be between 8 and 20 characters";
+  }
 
-    const isValid = Object.keys(errors).length === 0;
+  const isValid = Object.keys(errors).length === 0;
 
-    if (!isValid) {
-        const err = new Error("Invalid Payload");
-        err.statusCode = 422;
-        err.errors = errors;
-        throw err;
-    }
+  if (!isValid) {
+    const err = new Error("Invalid Payload");
+    err.statusCode = 422;
+    err.errors = errors;
+    throw err;
+  }
 }
